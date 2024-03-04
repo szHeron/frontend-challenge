386e21aca1e77e53384e54ed10b128ef6b21ad70
szheron <heronbr7@gmail.com>
2024-03-04 00:44:06 -0300

    feat: added dayjs

--- components/ForecastCard/index.tsx
+++ components/ForecastCard/index.tsx
@@ -2,17 +2,29 @@ import Image from "next/image";
 import { IWeatherForecast } from "@/app/page";
 import currentWeatherCondition from "@/utils/currentweathercondition";
 import formatDate from "@/utils/formatdate";
+import 'dayjs/locale/pt-br'
+import dayjs from "dayjs"
 
 export default function ForecastCard({data}:{data: IWeatherForecast}){
     const weather = data.weather[0].main.toLowerCase()
+
+    function dayName(){
+        const date = new Date(data.dt_txt);
+        const day = dayjs().day(date.getDay()).locale('pt-br').format("dddd");
+
+        return day.charAt(0).toUpperCase() + day.slice(1)
+    }
+    
     return (
         <div className="flex flex-row w-1/2 justify-between items-center">
             <div className="flex flex-col">
-                <p>Segunda</p>
+                <p>{dayName()}</p>
                 <p>{formatDate("date", data.dt_txt)}</p>
             </div>
-            <p>{data.main.temp.toFixed(0)}°</p>
-            <Image width={40} height={40} src={currentWeatherCondition(weather)} alt="ilustração do tempo"/>
+            <div className="flex flex-row gap-2 items-center">
+                <p>{data.main.temp.toFixed(0)}°</p>
+                <Image width={40} height={40} src={currentWeatherCondition(weather)} alt="ilustração do tempo"/>
+            </div>
         </div>
     )
 }
--- utils/formatdate.ts
+++ utils/formatdate.ts
@@ -1,14 +1,10 @@
-export default function formatDate(type:string, dateTxt:string) {
+import dayjs from "dayjs";
 
+export default function formatDate(type:string, dateTxt:string) {
     const date = new Date(dateTxt);
     if(type === "time"){
-        const formattedHours = date.getHours().toString().padStart(2, "0");
-        const formattedMinutes = date.getMinutes().toString().padStart(2, "0");
-        return `${formattedHours}:${formattedMinutes}`;
+        return dayjs(date).format("hh:mm")
     }
-
-    const formattedDay = date.getDate().toString().padStart(2, "0");
-    const formattedMonth = (date.getMonth() + 1).toString().padStart(2, "0");
     
-    return `${formattedDay}/${formattedMonth}`;
+    return dayjs(date).format("DD/MM");
 }
